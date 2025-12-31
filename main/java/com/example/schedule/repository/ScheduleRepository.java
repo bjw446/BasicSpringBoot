8c3f51dc9eb948d2917d4ecbf468009c17f3441a
bjw446 <bjw446@naver.com>
2025-12-31 19:32:31 +0900

    스케줄 CRUD2

--- main/java/com/example/schedule/repository/ScheduleRepository.java
+++ main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -1,6 +1,5 @@
 package com.example.schedule.repository;
 
-import com.example.schedule.dto.DeleteScheduleRequest;
 import com.example.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
